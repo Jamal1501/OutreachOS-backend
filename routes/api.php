c1bb0fc67964911eff22a1437487ff586dd36ee3
Jamal <hostheavens@gmail.com>
2026-03-06 12:05:53 +0100

    Enhance health check response and add new route

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApifyController;
 
 Route::get('/health', function () {
-    return response()->json(['status' => 'ok']);
+    return response()->json([
+        'status' => 'ok',
+        'service' => 'loveframes-outreach-api'
+    ]);
 });
+
+Route::post('/apify/run', [ApifyController::class, 'runTikTokHashtagActor']);
