343eab4880bfe73c253e7781d4b99bde43375b0d
Jamal <hostheavens@gmail.com>
2026-03-06 22:11:59 +0100

    Add CRM merge endpoint to API routes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('app.key')->group(function () {
     Route::get('/apify/status/{runId}', [ApifyController::class, 'getRunStatus']);
     Route::get('/apify/results/{datasetId}', [ApifyController::class, 'getDatasetResults']);
     Route::post('/apify/import-results', [ApifyController::class, 'importDatasetToSheet']);
+
     Route::post('/crm/merge-enriched', [ApifyController::class, 'mergeEnrichedToCreators']);
     Route::post('/tasks/generate', [ApifyController::class, 'generateTasks']);
     Route::post('/tasks/{taskId}/complete', [ApifyController::class, 'completeTask']);
