8525f4523bcffd771f467fc0b2f0d0df7ae9e42b
Jamal <hostheavens@gmail.com>
2026-03-06 22:09:20 +0100

    Add auth-check endpoint to API routes

--- routes/api.php
+++ routes/api.php
@@ -11,15 +11,16 @@ Route::get('/health', function () {
 });
 
 Route::middleware('app.key')->group(function () {
+    Route::get('/auth-check', function () {
+        return response()->json(['ok' => true]);
+    });
+
     Route::post('/apify/run', [ApifyController::class, 'runActor']);
     Route::get('/apify/status/{runId}', [ApifyController::class, 'getRunStatus']);
     Route::get('/apify/results/{datasetId}', [ApifyController::class, 'getDatasetResults']);
     Route::post('/apify/import-results', [ApifyController::class, 'importDatasetToSheet']);
-
     Route::post('/crm/merge-enriched', [ApifyController::class, 'mergeEnrichedToCreators']);
-
     Route::post('/tasks/generate', [ApifyController::class, 'generateTasks']);
     Route::post('/tasks/{taskId}/complete', [ApifyController::class, 'completeTask']);
-
     Route::post('/outreach/log', [ApifyController::class, 'logOutreachEvent']);
 });
