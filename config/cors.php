f445235da762294de27ca25dc8619e69011890a6
Jamal <hostheavens@gmail.com>
2026-03-26 15:47:37 +0100

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -2,17 +2,35 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
+
     'allowed_origins' => [
         'https://croos.lovable.app',
+        'https://loveframes-outreach-api-1.onrender.com',
+        'https://loveframes.shop',
+        'https://www.loveframes.shop',
     ],
+
     'allowed_origins_patterns' => [
         '#^https://[a-z0-9-]+\.lovable\.app$#',
+        '#^https://[a-z0-9-]+\.onrender\.com$#',
         '#^http://localhost(:\d+)?$#',
         '#^http://127\.0\.0\.1(:\d+)?$#',
     ],
-    'allowed_headers' => ['*'],
+
+    'allowed_headers' => [
+        'Accept',
+        'Authorization',
+        'Content-Type',
+        'Origin',
+        'X-Requested-With',
+        'X-APP-KEY',
+    ],
+
     'exposed_headers' => [],
-    'max_age' => 0,
+
+    'max_age' => 3600,
+
     'supports_credentials' => false,
 ];
