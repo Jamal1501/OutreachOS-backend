36e958e7f0f07c3f42accd5e3e30616dfebfa14d
Jamal <hostheavens@gmail.com>
2026-04-07 10:01:15 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -11,14 +11,15 @@ return [
         'https://loveframes.shop',
         'https://www.loveframes.shop',
         'http://127.0.0.1:8080',
-    'http://localhost:8080',
-    'http://192.168.2.218:8080',
+        'http://localhost:8080',
+        'http://192.168.2.218:8080',
     ],
 
     'allowed_origins_patterns' => [
         '#^https://[a-z0-9-]+\.lovable\.app$#',
         '#^https://[a-z0-9-]+\.lovableproject\.com$#',
         '#^https://[a-z0-9-]+\.onrender\.com$#',
+        '#^https://[a-z0-9-]+\.vercel\.app$#',
         '#^http://localhost(:\d+)?$#',
         '#^http://127\.0\.0\.1(:\d+)?$#',
     ],
