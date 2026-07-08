918993b6e6501b6d10e41aec0572960bdf145e64
Jamal1501 <hostheavens@gmail.com>
2026-07-08 22:50:26 +0200

    CSP installment

--- config/security.php
+++ config/security.php
@@ -3,7 +3,7 @@
 return [
     'csp' => [
         'enabled' => env('SECURITY_CSP_ENABLED', true),
-        'report_only' => env('SECURITY_CSP_REPORT_ONLY', true),
+        'report_only' => env('SECURITY_CSP_REPORT_ONLY', false),
         'report_uri' => env('SECURITY_CSP_REPORT_URI', '/api/csp-report'),
         'connect_src' => array_filter(array_map('trim', explode(',', env(
             'SECURITY_CSP_CONNECT_SRC',
